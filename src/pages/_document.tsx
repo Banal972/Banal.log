357be274aa58c0be70e1c1dcdd473ab1521ee960
Kim Ji Yu <spbabo97@naver.com>
2024-08-24 18:02:43 +0900

    chore : meta 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,15 +3,6 @@ import { Head, Html, Main, NextScript } from "next/document"
 const Document = () => {
   return (
     <Html lang="ko" className="h-full">
-      <Head>
-        <meta property="og:url" content="banal972.github.io" />
-        <meta property="og:title" content="Banal.log" />
-        <meta
-          property="og:description"
-          content="심플하게 만들어 노트처럼 작성한 프론트엔드 개발자의 회고록 Banal.log"
-        />
-        <meta property="og:image" content="" />
-      </Head>
       <body className="mx-auto max-w-2xl bg-[#fafaea] px-5 py-14">
         <Main />
         <footer className="mt-40 text-center text-gray-600">
